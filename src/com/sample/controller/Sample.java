058b099e7c410d3ae55ba6842982bd0a8e80ebc5
kamalesh <kamalez.sr@gmail.com>
2019-01-12 00:15:58 +0530

    first commit

---
+++ src/com/sample/controller/Sample.java
@@ -0,0 +1,13 @@
+package com.sample.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class Sample {
+	@RequestMapping(value="/hello",method=RequestMethod.GET)
+	public String WelcomeSpring(ModelMap model){
+		model.addAttribute("message","Hello Spring MVC Framework");
+		return "Welcome";
+	}
+}
